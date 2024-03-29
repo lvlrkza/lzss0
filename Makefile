b6870217f3e897cf5b5fba590e98431dafbcf1c2
lvlrk <lvlrk4u@proton.me>
2024-03-28 22:44:46 -0500

    initial

---
+++ Makefile
@@ -0,0 +1,19 @@
+NAME=lzss0
+EXT=so
+
+ifeq ($(OS),Windows_NT)
+EXT=dll
+endif
+
+TARGET=$(NAME).$(EXT)
+
+all: $(TARGET)
+
+$(TARGET):
+	python setup.py build_ext --inplace
+	mv *.$(EXT) $(TARGET)
+
+clean:
+	rm -rf $(TARGET) build lzss0.c
+
+.PHONY: all clean
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+
+setup(
+    name="lzss0",
+    version="1.0",
+    author="lvlrk",
+    author_email="lvlrk4u@proton.me",
+    description="A quick & feature-rich LZSS implementation",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python 3",
+        "License :: GNU GPL3",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.0",
+    ext_modules =
+        cythonize("lzss0.pyx", compiler_directives=
+                  {"language_level": "3"})
+)
